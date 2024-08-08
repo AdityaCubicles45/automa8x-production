489b37a4e11a178b02f0639ac35303fa5c18f689
Meet2054 <meetaparekh2003@gmail.com>
2024-08-08 21:04:34 +0530

    new update

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -14,7 +14,8 @@ import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 const Navbar =  (props: Props) => {
   // const user = await currentUser()
 const wallets = [new PetraWallet()];
-const {account} = useWallet();
+const {account,connected} = useWallet();
+
   return (
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
